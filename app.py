7f2a5af383b6bbdb78aa793954cc7cc86eac1658
Venaktesh Kannan <venkatesprasad512@gmail.com>
2022-02-06 02:33:02 +0000

    alive

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-@app.route('/webex/callback', methods=['POST'])
+@app.route('/webex/callback', methods=['GET','POST'])
 def webex_callback():
-    print(request.json)
-    print('hello World')
-    return Response(None, 200)
+    if request.method == 'POST':
+        print(request.json)
+        print('hello World')
+        return Response(None, 200)
+    else:
+        return Response("I'm alive", 200)
