b6b90db57c7bddc4cadbe1091d092ab0ad900850
Venaktesh Kannan <venkatesprasad512@gmail.com>
2022-02-06 03:03:07 +0000

    Asda

--- events.py
+++ events.py
@@ -19,8 +19,8 @@ class Person(db.Model):
 def webex_callback():
     if request.method == 'POST':
         reqData = request.json
-        print(reqData.data.id)
-        print(reqData.data.personEmail)
+        print(reqData['data']['id'])
+        print(reqData['data']['personEmail'])
         return Response('OK', 200)
     else:
         return Response("I'm alive", 200)
