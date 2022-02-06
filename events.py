9a61de184d5612d1b40905369e8e1bc448378ab3
Venaktesh Kannan <venkatesprasad512@gmail.com>
2022-02-06 02:57:18 +0000

    sad

--- events.py
+++ events.py
@@ -1,14 +1,23 @@
 from urllib import response
 from flask import Flask, Response, request, json
+from sqlalchemy.dialects.postgresql import UUID
 from os import environ
+from flask_sqlalchemy import SQLAlchemy
+import uuid
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True)
+    personal_email = db.Column(db.String(320), nullable=False)
+
 
 @app.route('/webex/callback', methods=['GET','POST'])
 def webex_callback():
     if request.method == 'POST':
-        reqData = json.loads(response.json)
+        reqData = json.loads(request.json)
         print(reqData.data.id)
         print(reqData.data.personEmail)
         return Response('OK', 200)
