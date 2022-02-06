e53402c3c9b6c84eb2dda252d840e2e6909a7638
Venaktesh Kannan <venkatesprasad512@gmail.com>
2022-02-06 02:17:11 +0000

    post

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route('/webex/callback')
+@app.route('/webex/callback', methods=['POST'])
 def webex_callback():
     return Response(None, 201)
