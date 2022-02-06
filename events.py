92be8c3d94cea2ac424061c1cd40999c98556ecd
Venaktesh Kannan <venkatesprasad512@gmail.com>
2022-02-06 04:04:34 +0000

    asd

--- events.py
+++ events.py
@@ -1,20 +1,25 @@
+from socket import socket
 from urllib import response
 from flask import Flask, Response, request, json
 from os import environ
 from flask_sqlalchemy import SQLAlchemy
+from flask_ngrok import run_with_ngrok
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL_1')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-db.create_all()
+run_with_ngrok(app)
+socketio = SocketIO(app)
 
 class Person(db.Model):
     id = db.Column(db.String, primary_key=True)
     personal_email = db.Column(db.String(320), nullable=False)
 
+db.create_all()
 
-@app.route('/webex/callback', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def webex_callback():
     if request.method == 'POST':
         reqData = request.json
@@ -26,3 +31,14 @@ def webex_callback():
         return Response('OK', 200)
     else:
         return Response("I'm alive", 200)
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message' + data)
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + json)
+
+if __name__ == '__main__':
+    app.run()
